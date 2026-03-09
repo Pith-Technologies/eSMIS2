bac4fcc2cc505995691745d3c543d163a77f2ca2
Pith Technologies <edwin@pithtech.net>
2026-03-09 21:47:15 +0800

    feat(starter): add esmis_starter module as application entry point
    Single install point for all eSMIS foundation modules. Sets Philippine
    localization defaults (PHP currency, PH country, Asia/Manila timezone).

---
+++ esmis_starter/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "eSMIS",
+    "version": "19.0.1.0.0",
+    "category": "eSMIS",
+    "summary": "Student Management Information System for Philippine HEIs",
+    "author": "Your Organization",
+    "website": "",
+    "license": "LGPL-3",
+    "development_status": "Alpha",
+    "maintainers": [],
+    "depends": [
+        "esmis_base",
+        "esmis_vocabulary",
+        "esmis_security",
+        "esmis_consent",
+        "esmis_approval",
+    ],
+    "data": [
+        "data/res_company_data.xml",
+    ],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+}
